d90ebd011620956b87d950ff69a6742a1db80b63
Shawn McElroy <shawn@skift.io>
2015-08-13 17:11:33 -0700

    for use with tutum

--- update.sh
+++ update.sh
@@ -5,15 +5,7 @@ current="$(curl -sSL 'http://api.wordpress.org/core/version-check/1.7/' | sed -r
 
 sha1="$(curl -sSL "https://wordpress.org/wordpress-$current.tar.gz.sha1")"
 
-for variant in apache fpm; do
-	(
-		set -x
-
-		sed -ri '
-			s/^(ENV WORDPRESS_VERSION) .*/\1 '"$current"'/;
-			s/^(ENV WORDPRESS_SHA1) .*/\1 '"$sha1"'/;
-		' "$variant/Dockerfile"
-
-		cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
-	)
-done
+sed -ri '
+	s/^(ENV WORDPRESS_VERSION) .*/\1 '"$current"'/;
+	s/^(ENV WORDPRESS_SHA1) .*/\1 '"$sha1"'/;
+' "$variant/Dockerfile"
